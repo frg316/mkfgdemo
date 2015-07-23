ccf14371691a69e42e1ee64b58739a4d2b9d4410
frg316 <frg316@lehigh.edu>
2015-07-22 21:03:16 -0400

    Latitude and Longitude

--- app/database/seeds/CommentTableSeeder.php
+++ app/database/seeds/CommentTableSeeder.php
@@ -9,17 +9,26 @@ class CommentTableSeeder extends Seeder
 
 		Comment::create(array(
 			'author' => 'Chris Sevilleja',
-			'text' => 'Look I am a test comment.'
+			'text' => 'Look I am a test comment.',
+			'location' => 'New York, US',
+			'latitude' => '40.7127 N',
+			'longitude' => '74.0059 W'
 		));
 
 		Comment::create(array(
 			'author' => 'Nick Cerminara',
 			'text' => 'This is going to be super crazy.'
+			'location' => 'Orlando, US',
+			'latitude' => '28.4158 N',
+			'longitude' => '81.2989 W'
 		));
 
 		Comment::create(array(
 			'author' => 'Holly Lloyd',
 			'text' => 'I am a master of Laravel and Angular.'
+			'location' => 'San Francisco, US'
+			'latitude' => '37.7833 N',
+			'longitude' => '122.4167 W'
 		));
 	}
 
